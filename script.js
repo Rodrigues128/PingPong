d32f19ed3733ecd7e2cef99a23b908500cd011dc
Paulo <paulo.h.r.correa@gmail.com>
2024-12-12 23:28:39 -0300

    2: desenho da linha central

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const canvasEl = document.querySelector("canvas"), canvasCtx = canvasEl.getContext("2d");
+const linewidth = 15;
 
 const setup = () => {
     canvasEl.width = canvasCtx.width = window.innerWidth;
@@ -6,6 +7,21 @@ const setup = () => {
 };
 
 const draw = () => {
+    // Desenho do campo
+    canvasCtx.fillStyle = "#286047";
+    canvasCtx.fillRect(0, 0, innerWidth, innerHeight);
+
+    
+    // Desenho da linha central
+    const x = innerWidth / 2 - linewidth / 2;
+    const y = 0;
+    const w = linewidth;
+    const h = innerHeight
+    
+    canvasCtx.fillStyle = "#fff";
+    canvasCtx.fillRect(x, y, w, h);
+
+    // Desenho das raquetes
 };
 
 setup();
