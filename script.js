84db74a532e59a0dda67157e685023d5cf3ad695
Paulo <paulo.h.r.correa@gmail.com>
2024-12-13 09:49:05 -0300

    Raquete e bolonha animada

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 const canvasEl = document.querySelector("canvas"), canvasCtx = canvasEl.getContext("2d"), gapX = 10;
 
+const mouse = {
+    x: 0,
+    y: 0,
+};
+
 const field = {
     w: innerWidth,
     h: innerHeight,
@@ -20,12 +25,16 @@ const line = {
 
 const leftPaddle = {
     x: gapX,
-    y: 100,
+    y: 0,
     w: line.w,
     h: 200,
+    _move: function () {
+        this.y = mouse.y - this.h / 2;
+    },
     draw: function () {
         canvasCtx.fillStyle = "#fff";
         canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+        this._move();
     },
 };
 
@@ -33,10 +42,14 @@ const rightPaddle = {
     x: field.w - line.w - gapX,
     y: 100,
     w: line.w,
-    h: 200,
+    h: 200,    
+    _move: function () {
+        this.y = ball.y;
+    },
     draw: function () {
         canvasCtx.fillStyle = "#fff";
         canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+        this._move;
     },
 };
 
@@ -57,11 +70,18 @@ const ball = {
     x: 300,
     y: 200,
     r: 20,
+    sepeed: 5,
+    _move: function () {
+        this.x += 1 * this.sepeed;
+        this.y += 1 * this.sepeed;
+    },
     draw: function () {
         canvasCtx.fillStyle = "#fff";
         canvasCtx.beginPath();
         canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
         canvasCtx.fill();
+
+        this._move()
     },
 };
 
@@ -79,5 +99,28 @@ const draw = () => {
     ball.draw();
 };
 
+window.animateFrame = (function () {
+    return (
+        window.requestAnimationFrame ||
+        window.webkitRequestAnimationFrame ||
+        window.mozRequestAnimationFrame ||
+        window.oRequestAnimationFrame ||
+        window.msRequestAnimationFrame ||
+        function (callback) {
+            return window.setTimeout(callback, 1000 / 60)
+        }
+    );
+})();
+
+const main = () => {
+    animateFrame(main);
+    draw();
+};
+
 setup();
-draw();
+main();
+
+canvasEl.addEventListener('mousemove', function(e) {
+    mouse.x = e.pageX;
+    mouse.y = e.pageY;
+})
