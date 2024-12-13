cac29868750e6eee3cd5fa0e1cdb728e4e96d28d
Paulo <paulo.h.r.correa@gmail.com>
2024-12-13 09:16:20 -0300

    transformando td em objeto

--- script.js
+++ script.js
@@ -1,37 +1,82 @@
-const canvasEl = document.querySelector("canvas"), canvasCtx = canvasEl.getContext("2d");
-const linewidth = 15;
+const canvasEl = document.querySelector("canvas"), canvasCtx = canvasEl.getContext("2d"), gapX = 10;
+
+const field = {
+    w: innerWidth,
+    h: innerHeight,
+    draw: function () {
+        canvasCtx.fillStyle = "#286047";
+        canvasCtx.fillRect(0, 0, this.w, this.h);
+    },
+};
+
+const line = {
+    w: 15,
+    h: field.h,
+    draw: function () {
+        canvasCtx.fillStyle = "#fff";
+        canvasCtx.fillRect(field.w / 2 - this.w / 2, 0, this.w, field.h);
+    },
+};
+
+const leftPaddle = {
+    x: gapX,
+    y: 100,
+    w: line.w,
+    h: 200,
+    draw: function () {
+        canvasCtx.fillStyle = "#fff";
+        canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+    },
+};
+
+const rightPaddle = {
+    x: field.w - line.w - gapX,
+    y: 100,
+    w: line.w,
+    h: 200,
+    draw: function () {
+        canvasCtx.fillStyle = "#fff";
+        canvasCtx.fillRect(this.x, this.y, this.w, this.h);
+    },
+};
+
+const score = {
+    human: 1,
+    computer: 2,
+    draw: function () {
+        canvasCtx.font = "bold 72px Arial";
+        canvasCtx.textAlign = "center";
+        canvasCtx.textBaseline = "top";
+        canvasCtx.fillStyle = "#01341D"
+        canvasCtx.fillText(this.human, field.w / 4, 50);
+        canvasCtx.fillText(this.computer, field.w / 2 + field.w / 4, 50);
+    }
+};
+
+const ball = {
+    x: 300,
+    y: 200,
+    r: 20,
+    draw: function () {
+        canvasCtx.fillStyle = "#fff";
+        canvasCtx.beginPath();
+        canvasCtx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
+        canvasCtx.fill();
+    },
+};
 
 const setup = () => {
-    canvasEl.width = canvasCtx.width = window.innerWidth;
-    canvasEl.height = canvasCtx.height = window.innerHeight;
+    canvasEl.width = canvasCtx.width = field.w;
+    canvasEl.height = canvasCtx.height = field.h;
 };
 
 const draw = () => {
-    // desenho do campo
-    canvasCtx.fillStyle = "#286047";
-    canvasCtx.fillRect(0, 0, innerWidth, innerHeight);
-
-
-    // desenho da linha central
-    canvasCtx.fillStyle = "#fff";
-    canvasCtx.fillRect(innerWidth / 2 - linewidth / 2, 0, linewidth, innerHeight);
-
-    // desenho das raquetes
-    canvasCtx.fillRect(10, 400, linewidth, 200); //esquerda
-    canvasCtx.fillRect(innerWidth - linewidth - 10, 400, linewidth, 200); // direita 
-
-    // desenho da bolinha
-    canvasCtx.beginPath();
-    canvasCtx.arc(200, 300, 20, 0, 2 * Math.PI, false);
-    canvasCtx.fill();
-
-    // desenho do placar
-    canvasCtx.font = "bold 72px Arial";
-    canvasCtx.textAlign = "center";
-    canvasCtx.textBaseline = "top";
-    canvasCtx.fillStyle = "#01341D"
-    canvasCtx.fillText("3", innerWidth / 4, 50);
-    canvasCtx.fillText("3", innerWidth / 2 + innerWidth / 4, 50);
+    field.draw();
+    line.draw();
+    leftPaddle.draw();
+    rightPaddle.draw();
+    score.draw();
+    ball.draw();
 };
 
 setup();
